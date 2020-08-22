331f1057c6e635eff94b45e8ee23395c3ce36a01
Luan Nguyen <luandcnd@gmail.com>
2020-08-22 21:55:32 +0700

    update add file

---
+++ bin/lang_config.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+class LangConfig {
+  static const KEYCODE_COLUMN_NAME = 'name';
+  static const HEADER_ROW_INDEX = 0;
+  static const TEXT_NAME_CLASS_FILE_NAME = 'text_names.dart';
+
+  static const TEXT_NAME_CLASS = 'TextName';
+
+  static String get xlsxDocPath => Directory.current.path + '/assets/';
+  static String get generateDirectory => Directory.current.path + '/generated/';
+}
